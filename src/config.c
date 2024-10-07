1a269d9520629ee90d6f90ebb0718a5972db6656
hwware <wen.hui.ware@gmail.com>
2024-10-07 09:29:56 +0000

    Add test case and Address Ping Comment
    Signed-off-by: hwware <wen.hui.ware@gmail.com>

--- src/config.c
+++ src/config.c
@@ -2499,7 +2499,7 @@ static int updateMaxmemoryReserved(const char **err) {
             server.maxmemory_reserved_scale = 60;
         }
     }
-    calculateMaxAvailableMemory();
+    updateMaxAvailableMemory();
     return 1;
 }
 
@@ -2514,7 +2514,7 @@ static int updateMaxmemory(const char **err) {
                       "depending on the maxmemory-policy.",
                       server.maxmemory, used);
         }
-        calculateMaxAvailableMemory();
+        updateMaxAvailableMemory();
         startEvictionTimeProc();
     }
     return 1;
