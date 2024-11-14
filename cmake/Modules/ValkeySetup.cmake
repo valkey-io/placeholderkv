1f26c5d79e32939f62591baca2cec547be314dfd
ranshid <ranshid@amazon.com>
2024-11-14 10:18:01 +0000

    Simplify the FORCE_DEFRAG Cmake logic
    Signed-off-by: ranshid <ranshid@amazon.com>

--- cmake/Modules/ValkeySetup.cmake
+++ cmake/Modules/ValkeySetup.cmake
@@ -168,11 +168,6 @@ endif ()
 
 message(STATUS "Using ${MALLOC_LIB}")
 
-# DFERAG Override
-if (FORCE_DEFRAG)
-    add_valkey_server_compiler_options("-DHAVE_DEFRAG -DFORCE_DEFRAG")
-endif ()
-
 # TLS support
 if (BUILD_TLS)
     valkey_parse_build_option(${BUILD_TLS} USE_TLS)
@@ -384,4 +379,3 @@ unset(BUILD_MALLOC CACHE)
 unset(USE_JEMALLOC CACHE)
 unset(BUILD_TLS_MODULE CACHE)
 unset(BUILD_TLS_BUILTIN CACHE)
-unset(FORCE_DEFRAG CACHE)
