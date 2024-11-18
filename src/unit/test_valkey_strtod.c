43696b5022372a5d71bb493a6b649d2db76f50c2
Madelyn Olson <madelyneolson@gmail.com>
2024-11-17 20:28:24 -0800

    Remove some unnecessary imports and use SPDX for license
    Signed-off-by: Madelyn Olson <madelyneolson@gmail.com>

--- src/unit/test_valkey_strtod.c
+++ src/unit/test_valkey_strtod.c
@@ -1,19 +1,11 @@
 /*
- * Copyright (c) 2024-present, Valkey contributors
+ * Copyright Valkey Contributors.
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- *   * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-License-Identifier: BSD 3-Clause
  */
 
 
 #include "../valkey_strtod.h"
-#include "assert.h"
 #include "errno.h"
 #include "math.h"
 #include "test_help.h"
@@ -23,28 +15,26 @@ int test_valkey_strtod(int argc, char **argv, int flags) {
     UNUSED(argv);
     UNUSED(flags);
 
-#ifdef USE_FAST_FLOAT
     double value = 0;
     errno = 0;
     valkey_strtod("231.2341234", &value);
-    assert(value == 231.2341234);
-    assert(errno == 0);
+    TEST_ASSERT(value == 231.2341234);
+    TEST_ASSERT(errno == 0);
 
     value = 0;
     valkey_strtod("+inf", &value);
-    assert(isinf(value));
-    assert(errno == 0);
+    TEST_ASSERT(isinf(value));
+    TEST_ASSERT(errno == 0);
 
     value = 0;
     valkey_strtod("-inf", &value);
-    assert(isinf(value));
-    assert(errno == 0);
+    TEST_ASSERT(isinf(value));
+    TEST_ASSERT(errno == 0);
 
     value = 0;
     valkey_strtod("inf", &value);
-    assert(isinf(value));
-    assert(errno == 0);
-#endif
+    TEST_ASSERT(isinf(value));
+    TEST_ASSERT(errno == 0);
 
     return 0;
 }
