ad212eb256faf72e3a5b2c165fad6aea15657498
Binbin <binloveplay1314@qq.com>
2024-11-21 20:32:36 +0800

    remove tmp file
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- cmake-build-debug/jemalloc-build/bin/jemalloc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-prefix=/Users/zhubinbin/github/valkey/cmake-build-debug/jemalloc-build
-exec_prefix=/Users/zhubinbin/github/valkey/cmake-build-debug/jemalloc-build
-libdir=${exec_prefix}/lib
-
-DYLD_INSERT_LIBRARIES=${libdir}/libjemalloc.2.dylib
-export DYLD_INSERT_LIBRARIES
-exec "$@"
