be4a5d75199a90acf232ef1907f1fed84734d421
Vadym Khoptynets <vadymkh@amazon.com>
2024-11-27 15:36:41 +0000

    Dismiss SDS instead of string object
    This will capture more cases. E.g. hashes.
    
    Signed-off-by: Vadym Khoptynets <vadymkh@amazon.com>

--- src/object.c
+++ src/object.c
@@ -389,17 +389,17 @@ void decrRefCount(robj *o) {
     }
 }
 
-/* See dismissObject() */
+/* See dismissObject(). sds is an exception. Instead of dismissing it
+ * with madvise(MADV_DONTNEED) we free it via the allocator. This has
+ * advantage that it allows the allocator to accumulate free buffers
+ * to free whole pages. While madvise is nop if the buffer is less
+ * than a page. The allocator overhead is minimal, because raw strings
+ * (sds) use the fast path. */
 void dismissSds(sds s) {
-    dismissMemory(sdsAllocPtr(s), sdsAllocSize(s));
+    sdsfree(s);
 }
 
-/* See dismissObject(). String object is an exception. Instead of
- * dismissing it with madvise(MADV_DONTNEED) we free it via the
- * allocator. This has advantage that it allows the allocator to
- * accumulate free buffers to free whole pages. While madvise is nop
- * if the buffer is less than a page. The allocator overhead is
- * minimal, because raw strings (sds) use the fast path. */
+/* See dismissObject() */
 void dismissStringObject(robj *o) {
     freeStringObject(o);
 }
