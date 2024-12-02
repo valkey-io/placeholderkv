85904dda908473510671acc75e1e4d643c3a3844
Pierre Turin <pieturin@amazon.com>
2024-12-02 19:53:27 +0000

    Free outbound link on MEET packet only after handshake timeout
    Signed-off-by: Pierre Turin <pieturin@amazon.com>

--- src/cluster_legacy.c
+++ src/cluster_legacy.c
@@ -3240,10 +3240,12 @@ int clusterProcessPacket(clusterLink *link) {
                  * the gossip section here since we have to trust the sender because
                  * of the message type. */
                 clusterProcessGossipSection(hdr, link);
-            } else if (sender->link) {
+            } else if (sender->link && now - sender->ctime > server.cluster_node_timeout) {
                 /* The MEET packet is from a known node, so the sender thinks that I do not know it.
                  * Freeing my outbound link to that node, to force a reconnect and sending a PING.
-                 * Once that node receives our PING, it should recognize the new connection as an inbound link from me. */
+                 * Once that node receives our PING, it should recognize the new connection as an inbound link from me.
+                 * We should only free the outbound link if the node is known for more time than the handshake timeout,
+                 * since during this time, the other side might still be trying to complete the handshake. */
                 serverAssert(link != sender->link); // we should always receive a MEET packet on an inbound link
                 serverLog(LL_NOTICE, "Freeing outbound link to node %.40s after receiving a MEET packet from this known node", sender->name);
                 freeClusterLink(sender->link);
