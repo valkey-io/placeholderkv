6cecf30a69d009ffdc94d32788ea2f317abcb7b0
uriyage <78144248+uriyage@users.noreply.github.com>
2024-12-16 09:56:03 +0200

    Update src/networking.c
    Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>
    Signed-off-by: uriyage <78144248+uriyage@users.noreply.github.com>

--- src/networking.c
+++ src/networking.c
@@ -4345,7 +4345,7 @@ size_t getClientMemoryUsage(client *c, size_t *output_buffer_mem_usage) {
 
     /* Add memory overhead of pubsub channels and patterns. Note: this is just the overhead of the robj pointers
      * to the strings themselves because they aren't stored per client. */
-    mem += c->pubsub_data ? pubsubMemOverhead(c) : 0;
+    mem += pubsubMemOverhead(c);
 
     /* Add memory overhead of the tracking prefixes, this is an underestimation so we don't need to traverse the entire
      * rax */
