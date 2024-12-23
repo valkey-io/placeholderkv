Synctest-hub/valkey

Extract the scripting engine code from the functions unit
This commit creates a new unit for the scripting engine code by
extracting the existing code from the functions unit.

We're doing this refactor to prepare the code for runnning the `EVAL`
command using different scripting engines.

Signed-off-by: Ricardo Dias <ricardo.dias@percona.com>

src/engine.h
@@ -1,0 +1,71 @@
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+#include "server.h"
+
+// Forward declaration of the engine structure.
+typedef struct engine engine;
+
+/* ValkeyModule type aliases for scripting engine structs and types. */
+typedef ValkeyModuleScriptingEngineCtx engineCtx;
+typedef ValkeyModuleScriptingEngineFunctionCtx functionCtx;
+typedef ValkeyModuleScriptingEngineCompiledFunction compiledFunction;
+typedef ValkeyModuleScriptingEngineMethods engineMethods;
+
+/*
+ * Callback function used to iterate the list of engines registered in the
+ * engine manager.
+ *
+ * - `engine`: the engine in the current iteration.
+ *
+ * - `context`: a generic pointer to a context object.
+ *
+ * If the callback function returns 0, then the iteration is stopped
+ * immediately.
+ */
+typedef int (*engineIterCallback)(engine *engine, void *context);
+
+/*
+ * Engine manager API functions.
+ */
+int engineManagerInit(void);
+size_t engineManagerGetCacheMemory(void);
+size_t engineManagerGetNumEngines(void);
+size_t engineManagerGetMemoryUsage(void);
+int engineManagerRegisterEngine(const char *engine_name,
+                                ValkeyModule *engine_module,
+                                engineCtx *engine_ctx,
+                                engineMethods *engine_methods);
+int engineManagerUnregisterEngine(const char *engine_name);
+engine *engineManagerFind(sds engine_name);
+void engineManagerForEachEngine(engineIterCallback callback, void *context);
+
+/*
+ * Engine API functions.
+ */
+sds engineGetName(engine *engine);
+client *engineGetClient(engine *engine);
+ValkeyModule *engineGetModule(engine *engine);
+
+/*
+ * API to call engine callback functions.
+ */
+compiledFunction **engineCallCreateFunctionsLibrary(
+    engine *engine,
+    const char *code,
+    size_t timeout,
+    size_t *out_num_compiled_functions,
+    char **err);
+void engineCallFunction(engine *engine,
+                        functionCtx *func_ctx,
+                        client *caller,
+                        void *compiled_function,
+                        robj **keys,
+                        size_t nkeys,
+                        robj **args,
+                        size_t nargs);
+size_t engineCallGetUsedMemory(engine *engine);
+size_t engineCallGetFunctionMemoryOverhead(engine *engine, void *compiled_function);
+void engineCallFreeFunction(engine *engine, void *compiled_func);
+
+#endif /* _ENGINE_H_ */

src/module.c
@@ -62,7 +62,7 @@
 #include "crc16_slottable.h"
 #include "valkeymodule.h"
 #include "io_threads.h"
-#include "functions.h"
+#include "engine.h"
 #include <dlfcn.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -13123,10 +13123,10 @@ int VM_RegisterScriptingEngine(ValkeyModuleCtx *ctx,
         return VALKEYMODULE_ERR;
     }
 
-    if (functionsRegisterEngine(engine_name,
-                                ctx->module,
-                                engine_ctx,
-                                engine_methods) != C_OK) {
+    if (engineManagerRegisterEngine(engine_name,
+                                    ctx->module,
+                                    engine_ctx,
+                                    engine_methods) != C_OK) {
         return VALKEYMODULE_ERR;
     }
 
@@ -13142,7 +13142,9 @@ int VM_RegisterScriptingEngine(ValkeyModuleCtx *ctx,
  */
 int VM_UnregisterScriptingEngine(ValkeyModuleCtx *ctx, const char *engine_name) {
     UNUSED(ctx);
-    functionsUnregisterEngine(engine_name);
+    if (engineManagerUnregisterEngine(engine_name) != C_OK) {
+        return VALKEYMODULE_ERR;
+    }
     return VALKEYMODULE_OK;
 }
 

valkey-io/placeholderkv

src/engine.h
@@ -1,0 +1,72 @@
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+#include "server.h"
+
+// Forward declaration of the engine structure.
+typedef struct engine engine;
+
+/* ValkeyModule type aliases for scripting engine structs and types. */
+typedef ValkeyModuleScriptingEngineCtx engineCtx;
+typedef ValkeyModuleScriptingEngineFunctionCtx functionCtx;
+typedef ValkeyModuleScriptingEngineCompiledFunction compiledFunction;
+typedef ValkeyModuleScriptingEngineMemoryInfo engineMemoryInfo;
+typedef ValkeyModuleScriptingEngineMethods engineMethods;
+
+/*
+ * Callback function used to iterate the list of engines registered in the
+ * engine manager.
+ *
+ * - `engine`: the engine in the current iteration.
+ *
+ * - `context`: a generic pointer to a context object.
+ *
+ * If the callback function returns 0, then the iteration is stopped
+ * immediately.
+ */
+typedef int (*engineIterCallback)(engine *engine, void *context);
+
+/*
+ * Engine manager API functions.
+ */
+int engineManagerInit(void);
+size_t engineManagerGetCacheMemory(void);
+size_t engineManagerGetNumEngines(void);
+size_t engineManagerGetMemoryUsage(void);
+int engineManagerRegisterEngine(const char *engine_name,
+                                ValkeyModule *engine_module,
+                                engineCtx *engine_ctx,
+                                engineMethods *engine_methods);
+int engineManagerUnregisterEngine(const char *engine_name);
+engine *engineManagerFind(sds engine_name);
+void engineManagerForEachEngine(engineIterCallback callback, void *context);
+
+/*
+ * Engine API functions.
+ */
+sds engineGetName(engine *engine);
+client *engineGetClient(engine *engine);
+ValkeyModule *engineGetModule(engine *engine);
+
+/*
+ * API to call engine callback functions.
+ */
+compiledFunction **engineCallCreateFunctionsLibrary(
+    engine *engine,
+    const char *code,
+    size_t timeout,
+    size_t *out_num_compiled_functions,
+    char **err);
+void engineCallFunction(engine *engine,
+                        functionCtx *func_ctx,
+                        client *caller,
+                        void *compiled_function,
+                        robj **keys,
+                        size_t nkeys,
+                        robj **args,
+                        size_t nargs);
+void engineCallFreeFunction(engine *engine, void *compiled_func);
+size_t engineCallGetFunctionMemoryOverhead(engine *engine, void *compiled_function);
+engineMemoryInfo engineCallGetMemoryInfo(engine *engine);
+
+#endif /* _ENGINE_H_ */

src/module.c
@@ -62,7 +62,7 @@
 #include "crc16_slottable.h"
 #include "valkeymodule.h"
 #include "io_threads.h"
-#include "functions.h"
+#include "engine.h"
 #include <dlfcn.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -13125,10 +13125,10 @@ int VM_RegisterScriptingEngine(ValkeyModuleCtx *module_ctx,
         return VALKEYMODULE_ERR;
     }
 
-    if (functionsRegisterEngine(engine_name,
-                                module_ctx->module,
-                                engine_ctx,
-                                engine_methods) != C_OK) {
+    if (engineManagerRegisterEngine(engine_name,
+                                    module_ctx->module,
+                                    engine_ctx,
+                                    engine_methods) != C_OK) {
         return VALKEYMODULE_ERR;
     }
 
@@ -13144,7 +13144,9 @@ int VM_RegisterScriptingEngine(ValkeyModuleCtx *module_ctx,
  */
 int VM_UnregisterScriptingEngine(ValkeyModuleCtx *ctx, const char *engine_name) {
     UNUSED(ctx);
-    functionsUnregisterEngine(engine_name);
+    if (engineManagerUnregisterEngine(engine_name) != C_OK) {
+        return VALKEYMODULE_ERR;
+    }
     return VALKEYMODULE_OK;
 }
 