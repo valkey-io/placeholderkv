765a4869b8ac4dd0573afba2d04b6a03ac332f6b
Uri Yagelnik <uriy@amazon.com>
2025-01-06 11:18:43 +0000

    Fix merge conflict
    Signed-off-by: Uri Yagelnik <uriy@amazon.com>

--- src/module.h
+++ src/module.h
@@ -228,7 +228,6 @@ int moduleLateDefrag(robj *key, robj *value, unsigned long *cursor, monotime end
 void moduleDefragGlobals(void);
 void *moduleGetHandleByName(char *modulename);
 int moduleIsModuleCommand(void *module_handle, struct serverCommand *cmd);
-void initClientModuleData(client *c);
 void freeClientModuleData(client *c);
 
 #endif /* _MODULE_H_ */
