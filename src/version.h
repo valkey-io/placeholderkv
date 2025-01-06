647b844917a7dc619d9dec54954bada432bbd20a
Madelyn Olson <madelyneolson@gmail.com>
2025-01-05 23:03:37 -0800

    Add release notes for 8.0.2
    Signed-off-by: Madelyn Olson <madelyneolson@gmail.com>

--- src/version.h
+++ src/version.h
@@ -4,8 +4,8 @@
  * similar. */
 #define SERVER_NAME "valkey"
 #define SERVER_TITLE "Valkey"
-#define VALKEY_VERSION "8.0.1"
-#define VALKEY_VERSION_NUM 0x00080001
+#define VALKEY_VERSION "8.0.2"
+#define VALKEY_VERSION_NUM 0x00080002
 
 /* Redis OSS compatibility version, should never
  * exceed 7.2.x. */
