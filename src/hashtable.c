e9dcb59f52c6ab37cdcf9173b55a4fe8e6ac01f6
hengyouhai <hengyouhai@tuhu.cn>
2025-01-13 21:35:11 +0800

    Loop optimization: move maxlen check outside to reduce unnecessary checks
    Signed-off-by: hengyouhai <hengyouhai@tuhu.cn>

--- src/hashtable.c
+++ src/hashtable.c
@@ -2183,10 +2183,11 @@ int hashtableLongestBucketChain(hashtable *ht) {
             int chainlen = 0;
             bucket *b = &ht->tables[table][i];
             while (b->chained) {
-                if (++chainlen > maxlen) {
-                    maxlen = chainlen;
-                }
+                ++chainlen
                 b = getChildBucket(b);
+            }
+            if (chainlen > maxlen) {
+                maxlen = chainlen;
             }
         }
     }
