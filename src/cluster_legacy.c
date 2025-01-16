518b88ba1f2b960f1c6788339b76770b6072c411
hwware <wen.hui.ware@gmail.com>
2025-01-16 21:35:36 +0000

    add one variable initilzation
    Signed-off-by: hwware <wen.hui.ware@gmail.com>

--- src/cluster_legacy.c
+++ src/cluster_legacy.c
@@ -1134,6 +1134,7 @@ void clusterInit(void) {
     server.cluster->failover_auth_epoch = 0;
     server.cluster->cant_failover_reason = CLUSTER_CANT_FAILOVER_NONE;
     server.cluster->lastVoteEpoch = 0;
+    server.cluster->failover_auth_sent = 0;
 
     /* Initialize stats */
     for (int i = 0; i < CLUSTERMSG_TYPE_COUNT; i++) {
