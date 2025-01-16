2df98bebccc582b5d3c9dbbb00853b0921d94886
Binbin <binloveplay1314@qq.com>
2025-01-16 10:40:31 +0800

    Extend LATENCY LATEST to add min / sum / cnt stats
    Currently LATENCY LATEST only has the following fields:
    - Event name.
    - Unix timestamp of the latest latency spike for the event.
    - Latest event latency in millisecond.
    - All-time maximum latency for this event.
    
    This PR introduced these fields:
    - min: the all-time minimum latency for this event (maybe not that useful)
    - sum: the all-time sum latency for this event (i think it is useful for some events)
    - cnt: the event count that trigger the latency, with the sum we can calc the avg
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/latency.c
+++ src/latency.c
@@ -86,11 +86,17 @@ void latencyAddSample(const char *event, mstime_t latency) {
         ts = zmalloc(sizeof(*ts));
         ts->idx = 0;
         ts->max = 0;
+        ts->min = 0;
+        ts->sum = 0;
+        ts->cnt = 0;
         memset(ts->samples, 0, sizeof(ts->samples));
         dictAdd(server.latency_events, zstrdup(event), ts);
     }
 
     if (latency > ts->max) ts->max = latency;
+    if (latency < ts->min || ts->min == 0) ts->min = latency;
+    ts->sum += latency;
+    ts->cnt++;
 
     /* If the previous sample is in the same second, we update our old sample
      * if this latency is > of the old one, or just return. */
@@ -612,11 +618,14 @@ void latencyCommandReplyWithLatestEvents(client *c) {
         struct latencyTimeSeries *ts = dictGetVal(de);
         int last = (ts->idx + LATENCY_TS_LEN - 1) % LATENCY_TS_LEN;
 
-        addReplyArrayLen(c, 4);
+        addReplyArrayLen(c, 7);
         addReplyBulkCString(c, event);
         addReplyLongLong(c, ts->samples[last].time);
         addReplyLongLong(c, ts->samples[last].latency);
         addReplyLongLong(c, ts->max);
+        addReplyLongLong(c, ts->min);
+        addReplyLongLong(c, ts->sum);
+        addReplyLongLong(c, ts->cnt);
     }
     dictReleaseIterator(di);
 }
