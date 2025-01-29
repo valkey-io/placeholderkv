valkey-io/placeholderkv

commit 6828a4b8956896d3c3d0453af717866e76acfb8f

Two small fixes
Signed-off-by: Ricardo Dias <ricardo.dias@percona.com>

src/eval.c
@@ -83,7 +83,7 @@ dictType shaScriptObjectDictType = {
 
 /* Eval context */
 struct evalCtx {
-    dict *scripts;                  /* A dictionary of SHA1 -> scripts */
+    dict *scripts;                  /* A dictionary of SHA1 -> evalScript */
     list *scripts_lru_list;         /* A list of SHA1, first in first out LRU eviction. */
     unsigned long long scripts_mem; /* Cached scripts' memory + oh */
 } evalCtx;
@@ -271,8 +271,7 @@ int evalExtractShebangFlags(sds body,
     } else {
         // When no shebang is declared, assume the engine is LUA.
         if (out_engine) {
-            *out_engine = zcalloc(4);
-            valkey_strlcpy(*out_engine, "lua", 4);
+            *out_engine = zstrdup("dup");
         }
     }
     if (out_shebang_len) *out_shebang_len = shebang_len;

commit 806893f0b4feaad67c66ef725ad60bb6704ccf20

Fix bug introduced in 6828a4b
Signed-off-by: Ricardo Dias <ricardo.dias@percona.com>

src/eval.c
@@ -273,7 +273,7 @@ int evalExtractShebangFlags(sds body,
     } else {
         // When no shebang is declared, assume the engine is LUA.
         if (out_engine) {
-            *out_engine = zstrdup("dup");
+            *out_engine = zstrdup("lua");
         }
     }
     if (out_shebang_len) *out_shebang_len = shebang_len;